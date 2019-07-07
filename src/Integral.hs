1aceb24b7e2139897e99fd40503a4e9423da3e54
Robert Vollmert <rob@vllmrt.net>
2019-07-07 17:12:06 +0200

    don't use unicode in function definition

--- src/Integral.hs
+++ src/Integral.hs
@@ -5,7 +5,7 @@ module Integral
     number'
   ) where
 
-number' ∷ (Ord a, Integral a)
-        ⇒ a → p
+number' :: (Ord a, Integral a)
+        => a -> p
 number' = undefined
 {-# SPECIALIZE number' ∷ (Ord a, Integral a) ⇒ a → p #-}
