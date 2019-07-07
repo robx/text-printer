a6d634e0c722fb42c25d56c33cf75fd10a486247
Robert Vollmert <rob@vllmrt.net>
2019-07-07 17:03:58 +0200

    Revert "remove Integral constraint"
    This reverts commit 9533d6ce9a84ce5ad7869c3859c832b2e6b5c07f.

--- src/Integral.hs
+++ src/Integral.hs
@@ -5,6 +5,7 @@ module Integral
     number'
   ) where
 
-number' ∷ Ord α ⇒ p → p → p → α → p
+number' ∷ (Ord α, Integral α)
+        ⇒ p → p → p → α → p
 number' = undefined
-{-# SPECIALIZE number' ∷ Ord α ⇒ p → p → p → α → p #-}
+{-# SPECIALIZE number' ∷ (Ord α, Integral α) ⇒ p → p → p → α → p #-}
