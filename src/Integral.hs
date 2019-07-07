685a8a84866f363af05d0bff2704ee12323b24ed
Robert Vollmert <rob@vllmrt.net>
2019-07-07 19:57:26 +0200

    Revert "down to three"
    This reverts commit f23d20510b95cfcde5e51c2ec1731e3f405d7ae4.

--- src/Integral.hs
+++ src/Integral.hs
@@ -16,6 +16,9 @@ instance X B
 data C
 instance X C
 
+data D
+instance X D
+
 f ∷ (X s, Ord a, Integral a)
         => s
         -> p -- ^ Prefix for negative values
@@ -26,3 +29,4 @@ f = undefined
 {-# SPECIALIZE f ∷ (Ord a, Integral a) => A -> p -> p -> p -> a -> p #-}
 {-# SPECIALIZE f ∷ (Ord a, Integral a) => B -> p -> p -> p -> a -> p #-}
 {-# SPECIALIZE f ∷ (Ord a, Integral a) => C -> p -> p -> p -> a -> p #-}
+{-# SPECIALIZE f ∷ (Ord a, Integral a) => D -> p -> p -> p -> a -> p #-}
