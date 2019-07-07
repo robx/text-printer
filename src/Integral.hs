9517a5e585743ea4c490f2bf6bb590809f4b85ec
Robert Vollmert <rob@vllmrt.net>
2019-07-07 19:45:09 +0200

    Revert "remove lowhex/uphex"
    This reverts commit 32a49c42a1f470ccc4c244f5eea116e6a4f71c35.

--- src/Integral.hs
+++ src/Integral.hs
@@ -15,6 +15,14 @@ data Hexadecimal = Hexadecimal deriving ( Eq, Ord, Show, Read )
 
 instance PositionalSystem Hexadecimal
 
+data LowHex = LowHex deriving ( Eq, Ord, Show, Read )
+
+instance PositionalSystem LowHex
+
+data UpHex = UpHex deriving ( Eq, Ord, Show, Read )
+
+instance PositionalSystem UpHex 
+
 f ∷ (PositionalSystem s, Ord a, Integral a)
         => s
         -> p -- ^ Prefix for negative values
@@ -24,3 +32,5 @@ f ∷ (PositionalSystem s, Ord a, Integral a)
 f = undefined
 {-# SPECIALIZE f ∷ (Ord a, Integral a) => Decimal -> p -> p -> p -> a -> p #-}
 {-# SPECIALIZE f ∷ (Ord a, Integral a) => Hexadecimal -> p -> p -> p -> a -> p #-}
+{-# SPECIALIZE f ∷ (Ord a, Integral a) => LowHex -> p -> p -> p -> a -> p #-}
+{-# SPECIALIZE f ∷ (Ord a, Integral a) => UpHex -> p -> p -> p -> a -> p #-}
