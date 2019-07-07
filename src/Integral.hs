d7b4ec3b022ceb501487c9a362ac34f5b2d0ad21
Robert Vollmert <rob@vllmrt.net>
2019-07-07 19:43:19 +0200

    Revert "no more positional system"
    This reverts commit ded0beed1b277db264ad1a96cf59e3fdbbe80ac6.

--- src/Integral.hs
+++ src/Integral.hs
@@ -5,18 +5,32 @@ module Integral
     f
   ) where
 
+class PositionalSystem s
+
 data Decimal = Decimal deriving ( Eq, Ord, Show, Read )
+
+instance PositionalSystem Decimal
 
 data Hexadecimal = Hexadecimal deriving ( Eq, Ord, Show, Read )
 
+instance PositionalSystem Hexadecimal
+
 data LowHex = LowHex deriving ( Eq, Ord, Show, Read )
+
+instance PositionalSystem LowHex
 
 data UpHex = UpHex deriving ( Eq, Ord, Show, Read )
 
-f ∷ (Ord a, Integral a)
-        => p -> a
+instance PositionalSystem UpHex 
+
+f ∷ (PositionalSystem s, Ord a, Integral a)
+        => s
+        -> p -- ^ Prefix for negative values
+        -> p -- ^ Zero printer
+        -> p -- ^ Prefix for positive values
+        -> a -> p
 f = undefined
-{-# SPECIALIZE f ∷ (Ord a, Integral a) => Decimal -> a #-}
-{-# SPECIALIZE f ∷ (Ord a, Integral a) => Hexadecimal -> a #-}
-{-# SPECIALIZE f ∷ (Ord a, Integral a) => LowHex -> a #-}
-{-# SPECIALIZE f ∷ (Ord a, Integral a) => UpHex -> a #-}
+{-# SPECIALIZE f ∷ (Ord a, Integral a) => Decimal -> p -> p -> p -> a -> p #-}
+{-# SPECIALIZE f ∷ (Ord a, Integral a) => Hexadecimal -> p -> p -> p -> a -> p #-}
+{-# SPECIALIZE f ∷ (Ord a, Integral a) => LowHex -> p -> p -> p -> a -> p #-}
+{-# SPECIALIZE f ∷ (Ord a, Integral a) => UpHex -> p -> p -> p -> a -> p #-}
