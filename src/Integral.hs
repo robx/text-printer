97ac417a7dc84ee54e9da1cbb4dfe099b74fe67c
Robert Vollmert <rob@vllmrt.net>
2019-07-07 18:49:42 +0200

    single arrow

--- src/Integral.hs
+++ src/Integral.hs
@@ -12,9 +12,9 @@ import Data.String (IsString(..))
 import Data.Monoid (Monoid(..))
 
 class PositionalSystem s where
-  radixIn ∷ Num a => s → a
-  intToDigitIn ∷ s → Int → Char
-  printDigitIn ∷ Printer p => s → Char → p
+  radixIn ∷ Num a => s -> a
+  intToDigitIn ∷ s -> Int -> Char
+  printDigitIn ∷ Printer p => s -> Char -> p
   printDigitIn _ = char7
   {-# INLINE printDigitIn #-}
 
@@ -55,33 +55,33 @@ instance PositionalSystem UpHex where
 
 f ∷ (PositionalSystem s, Ord a, Integral a, Printer p)
         => s
-        → p -- ^ Prefix for negative values
-        → p -- ^ Zero printer
-        → p -- ^ Prefix for positive values
-        → a → p
+        -> p -- ^ Prefix for negative values
+        -> p -- ^ Zero printer
+        -> p -- ^ Prefix for positive values
+        -> a -> p
 f = undefined
-{-# SPECIALIZE f ∷ Printer p => Decimal → p → p → p → Int → p #-}
-{-# SPECIALIZE f ∷ Printer p => Decimal → p → p → p → Int8 → p #-}
-{-# SPECIALIZE f ∷ Printer p => Decimal → p → p → p → Int16 → p #-}
-{-# SPECIALIZE f ∷ Printer p => Decimal → p → p → p → Int32 → p #-}
-{-# SPECIALIZE f ∷ Printer p => Decimal → p → p → p → Int64 → p #-}
-{-# SPECIALIZE f ∷ Printer p => Decimal → p → p → p → Word → p #-}
-{-# SPECIALIZE f ∷ Printer p => Decimal → p → p → p → Word8 → p #-}
-{-# SPECIALIZE f ∷ Printer p => Decimal → p → p → p → Word16 → p #-}
-{-# SPECIALIZE f ∷ Printer p => Decimal → p → p → p → Word32 → p #-}
-{-# SPECIALIZE f ∷ Printer p => Decimal → p → p → p → Word64 → p #-}
-{-# SPECIALIZE f ∷ (Ord a, Integral a, Printer p) => Decimal → p → p → p → a → p #-}
-{-# SPECIALIZE f ∷ (Ord a, Integral a, Printer p) => Hexadecimal → p → p → p → a → p #-}
-{-# SPECIALIZE f ∷ (Ord a, Integral a, Printer p) => LowHex → p → p → p → a → p #-}
-{-# SPECIALIZE f ∷ (Ord a, Integral a, Printer p) => UpHex → p → p → p → a → p #-}
+{-# SPECIALIZE f ∷ Printer p => Decimal -> p -> p -> p -> Int -> p #-}
+{-# SPECIALIZE f ∷ Printer p => Decimal -> p -> p -> p -> Int8 -> p #-}
+{-# SPECIALIZE f ∷ Printer p => Decimal -> p -> p -> p -> Int16 -> p #-}
+{-# SPECIALIZE f ∷ Printer p => Decimal -> p -> p -> p -> Int32 -> p #-}
+{-# SPECIALIZE f ∷ Printer p => Decimal -> p -> p -> p -> Int64 -> p #-}
+{-# SPECIALIZE f ∷ Printer p => Decimal -> p -> p -> p -> Word -> p #-}
+{-# SPECIALIZE f ∷ Printer p => Decimal -> p -> p -> p -> Word8 -> p #-}
+{-# SPECIALIZE f ∷ Printer p => Decimal -> p -> p -> p -> Word16 -> p #-}
+{-# SPECIALIZE f ∷ Printer p => Decimal -> p -> p -> p -> Word32 -> p #-}
+{-# SPECIALIZE f ∷ Printer p => Decimal -> p -> p -> p -> Word64 -> p #-}
+{-# SPECIALIZE f ∷ (Ord a, Integral a, Printer p) => Decimal -> p -> p -> p -> a -> p #-}
+{-# SPECIALIZE f ∷ (Ord a, Integral a, Printer p) => Hexadecimal -> p -> p -> p -> a -> p #-}
+{-# SPECIALIZE f ∷ (Ord a, Integral a, Printer p) => LowHex -> p -> p -> p -> a -> p #-}
+{-# SPECIALIZE f ∷ (Ord a, Integral a, Printer p) => UpHex -> p -> p -> p -> a -> p #-}
 
 class (IsString p, Semigroup p, Monoid p) => Printer p where
-  char ∷ Char → p
+  char ∷ Char -> p
   char c = string [c]
   {-# INLINE char #-}
-  char7 ∷ Char → p
+  char7 ∷ Char -> p
   char7 = char
   {-# INLINE char7 #-}
-  string ∷ String → p
+  string ∷ String -> p
   string = fromString
   {-# INLINE string #-}
