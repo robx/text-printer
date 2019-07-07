ded0beed1b277db264ad1a96cf59e3fdbbe80ac6
Robert Vollmert <rob@vllmrt.net>
2019-07-07 19:38:08 +0200

    no more positional system

--- src/Integral.hs
+++ src/Integral.hs
@@ -5,32 +5,18 @@ module Integral
     f
   ) where
 
-class PositionalSystem s
-
 data Decimal = Decimal deriving ( Eq, Ord, Show, Read )
-
-instance PositionalSystem Decimal
 
 data Hexadecimal = Hexadecimal deriving ( Eq, Ord, Show, Read )
 
-instance PositionalSystem Hexadecimal
-
 data LowHex = LowHex deriving ( Eq, Ord, Show, Read )
-
-instance PositionalSystem LowHex
 
 data UpHex = UpHex deriving ( Eq, Ord, Show, Read )
 
-instance PositionalSystem UpHex 
-
-f ∷ (PositionalSystem s, Ord a, Integral a)
-        => s
-        -> p -- ^ Prefix for negative values
-        -> p -- ^ Zero printer
-        -> p -- ^ Prefix for positive values
-        -> a -> p
+f ∷ (Ord a, Integral a)
+        => p -> a
 f = undefined
-{-# SPECIALIZE f ∷ (Ord a, Integral a) => Decimal -> p -> p -> p -> a -> p #-}
-{-# SPECIALIZE f ∷ (Ord a, Integral a) => Hexadecimal -> p -> p -> p -> a -> p #-}
-{-# SPECIALIZE f ∷ (Ord a, Integral a) => LowHex -> p -> p -> p -> a -> p #-}
-{-# SPECIALIZE f ∷ (Ord a, Integral a) => UpHex -> p -> p -> p -> a -> p #-}
+{-# SPECIALIZE f ∷ (Ord a, Integral a) => Decimal -> a #-}
+{-# SPECIALIZE f ∷ (Ord a, Integral a) => Hexadecimal -> a #-}
+{-# SPECIALIZE f ∷ (Ord a, Integral a) => LowHex -> a #-}
+{-# SPECIALIZE f ∷ (Ord a, Integral a) => UpHex -> a #-}
