d062f98ff7ec7fe43c9947a6242b705401788d3e
Robert Vollmert <rob@vllmrt.net>
2019-07-07 19:14:22 +0200

    remove printer

--- src/Integral.hs
+++ src/Integral.hs
@@ -7,10 +7,6 @@ module Integral
 
 import Data.Int
 import Data.Word
-import Data.String (IsString(..))
-import Data.Monoid (Monoid(..))
-
-class (IsString p, Semigroup p, Monoid p) => Printer p
 
 class PositionalSystem s
 
@@ -30,24 +26,24 @@ data UpHex = UpHex deriving ( Eq, Ord, Show, Read )
 
 instance PositionalSystem UpHex 
 
-f ∷ (PositionalSystem s, Ord a, Integral a, Printer p)
+f ∷ (PositionalSystem s, Ord a, Integral a)
         => s
         -> p -- ^ Prefix for negative values
         -> p -- ^ Zero printer
         -> p -- ^ Prefix for positive values
         -> a -> p
 f = undefined
-{-# SPECIALIZE f ∷ Printer p => Decimal -> p -> p -> p -> Int -> p #-}
-{-# SPECIALIZE f ∷ Printer p => Decimal -> p -> p -> p -> Int8 -> p #-}
-{-# SPECIALIZE f ∷ Printer p => Decimal -> p -> p -> p -> Int16 -> p #-}
-{-# SPECIALIZE f ∷ Printer p => Decimal -> p -> p -> p -> Int32 -> p #-}
-{-# SPECIALIZE f ∷ Printer p => Decimal -> p -> p -> p -> Int64 -> p #-}
-{-# SPECIALIZE f ∷ Printer p => Decimal -> p -> p -> p -> Word -> p #-}
-{-# SPECIALIZE f ∷ Printer p => Decimal -> p -> p -> p -> Word8 -> p #-}
-{-# SPECIALIZE f ∷ Printer p => Decimal -> p -> p -> p -> Word16 -> p #-}
-{-# SPECIALIZE f ∷ Printer p => Decimal -> p -> p -> p -> Word32 -> p #-}
-{-# SPECIALIZE f ∷ Printer p => Decimal -> p -> p -> p -> Word64 -> p #-}
-{-# SPECIALIZE f ∷ (Ord a, Integral a, Printer p) => Decimal -> p -> p -> p -> a -> p #-}
-{-# SPECIALIZE f ∷ (Ord a, Integral a, Printer p) => Hexadecimal -> p -> p -> p -> a -> p #-}
-{-# SPECIALIZE f ∷ (Ord a, Integral a, Printer p) => LowHex -> p -> p -> p -> a -> p #-}
-{-# SPECIALIZE f ∷ (Ord a, Integral a, Printer p) => UpHex -> p -> p -> p -> a -> p #-}
+{-# SPECIALIZE f ∷ Decimal -> p -> p -> p -> Int -> p #-}
+{-# SPECIALIZE f ∷ Decimal -> p -> p -> p -> Int8 -> p #-}
+{-# SPECIALIZE f ∷ Decimal -> p -> p -> p -> Int16 -> p #-}
+{-# SPECIALIZE f ∷ Decimal -> p -> p -> p -> Int32 -> p #-}
+{-# SPECIALIZE f ∷ Decimal -> p -> p -> p -> Int64 -> p #-}
+{-# SPECIALIZE f ∷ Decimal -> p -> p -> p -> Word -> p #-}
+{-# SPECIALIZE f ∷ Decimal -> p -> p -> p -> Word8 -> p #-}
+{-# SPECIALIZE f ∷ Decimal -> p -> p -> p -> Word16 -> p #-}
+{-# SPECIALIZE f ∷ Decimal -> p -> p -> p -> Word32 -> p #-}
+{-# SPECIALIZE f ∷ Decimal -> p -> p -> p -> Word64 -> p #-}
+{-# SPECIALIZE f ∷ (Ord a, Integral a) => Decimal -> p -> p -> p -> a -> p #-}
+{-# SPECIALIZE f ∷ (Ord a, Integral a) => Hexadecimal -> p -> p -> p -> a -> p #-}
+{-# SPECIALIZE f ∷ (Ord a, Integral a) => LowHex -> p -> p -> p -> a -> p #-}
+{-# SPECIALIZE f ∷ (Ord a, Integral a) => UpHex -> p -> p -> p -> a -> p #-}
